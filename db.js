d23a1ac152b94e5ecf23cf7b0869d354bd01e016
vladimir-trifonov <trifonov.dev@gmail.com>
2018-09-16 15:14:41 +0300

    Code review fixes

--- db.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-module.exports.parties = () => {
-  let _parties = {}
-  return {
-    get: (partyId) => typeof partyId !== 'undefined' ? _parties[partyId] : _parties,
-    set: (partyId, party) => (_parties[partyId] = party)
-  }
-}
